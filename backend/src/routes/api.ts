e38ee7b1fc680b3b86c12bf179ca02e63e0a7b68
Daniil <baldindaniilza@mail.ru>
2024-11-30 21:34:07 +0500

    Сделал многопоточность (типа)

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,5 +1,4 @@
 import { Router, Request, Response } from "express";
-import axios, { AxiosResponse } from "axios";
 import multer from "multer";
 import { sendToMistral } from "../lib/mistral";
 import { MistralResponseContent } from "../lib/types";
@@ -35,32 +34,35 @@ router.post(
 			// Преобразуем содержимое файла в строку
 			const fileContent = fileBuffer.toString("utf-8");
 
-			// Отправка в нейронку
-			const messages: MistralResponseContent[] = [];
-
-			const time1 = Date.now() / 1000;
-			console.log("Sending first message...");
-			messages.push(
-				await sendToMistral(
+			// Список запросов
+			const requests: any[] = [
+				[
 					"Учитывай при проверке правила синтаксиса языка",
 					"Проверь этот программный код на соответствие заданным правилам. В ответе опиши только ошибки, связанные с некорректным синтаксисом.",
-					fileContent
-				)
-			);
-			console.log("Time to send first message: " + Math.floor(Date.now() / 1000 - time1) + " seconds");
-
-			const time2 = Date.now() / 1000;
-			console.log("Sending second message...");
-			messages.push(
-				await sendToMistral(
+					fileContent,
+				],
+				[
 					"Учитывай при проверке корректное именование переменных, функций и другого." +
 						"Именование должно быть осмысленное и подходить по контексту программного кода." +
 						"Не ДОЛЖНО быть ошибок в именах переменных, функций и другого.",
 					"Проверь этот программный код на соответствие заданным правилам. В ответе опиши только ошибки, связанные с некорректным именованием.",
-					fileContent
-				)
+					fileContent,
+				],
+			];
+
+			const time1 = Date.now() / 1000;
+			console.log("Sending request...");
+			
+			// Отправка в нейронку
+			const response: any[] =
+				await Promise.allSettled(
+					requests.map(async (value) => await sendToMistral(value[0], value[1], value[2]))
+				);
+			const messages = response.map((item) => item?.value || {});
+
+			console.log(
+				"Time to send request: " + Math.floor(Date.now() / 1000 - time1) + " seconds"
 			);
-			console.log("Time to send second message: " + Math.floor(Date.now() / 1000 - time2) + " seconds");
 
 			console.log("Sending response...");
 
